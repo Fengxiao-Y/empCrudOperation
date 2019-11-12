56310928ec703814832de7647765697eb211e9d1
Fengxiao-Y <56786092+Fengxiao-Y@users.noreply.github.com>
2019-11-12 16:23:14 +0800

    delete test

--- src/com/yidu/d280/domain/test01.java
+++
@@ -1,9 +0,0 @@
-package com.yidu.d280.domain;
-
-public class test01 {
-	public static void main(String[] args){
-		System.out.println("真滴有点傻！");
-		System.out.println("好像搞定了");
-		System.out.println("你到底要怎么样");
-	}
-}
--- src/com/yidu/d280/domain/test02.java
+++
@@ -1,9 +0,0 @@
-package com.yidu.d280.domain;
-
-public class test02 {
-	public static void main(String[] arge){
-		System.out.println("太难了");
-		System.out.println("这世道啊");
-		System.out.println("无情");
-	}
-}
